08b7bebe1b6902b356151bf0d3f64ad3f5d024bf
sabatauqeer1 <sabatauqeer860@gmail.com>
2025-09-15 14:50:08 +0500

    feat: implement GET /transactions route with optional type filter

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -1,22 +1,32 @@
 import { Transaction } from "../models/transactionModel.js";
 export const getTransaction = async (req, res) => {
+  const { type } = req.query;
+
   try {
-    const transaction = await Transaction.find({});
-    res.status(200).json({ transaction });
+    if (type === "expense") {
+      const transactions = await Transaction.find({ type: type });
+      res.status(200).json({ transactions });
+    }
+    if (type === "income") {
+      const transactions = await Transaction.find({ type: type });
+      res.status(200).json({ transactions });
+    }
+    const transactions = await Transaction.find({});
+    res.status(200).json({ transactions });
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch transactions" });
   }
 };
 export const addTransaction = async (req, res) => {
   const { amount, type, category, note } = req.body;
-  const transaction = await Transaction.create({
-    amount: amount,
-    type: type,
-    category: category,
-    note: note,
-  });
-  res.status(200).json({ transaction });
   try {
+    const transaction = await Transaction.create({
+      amount: amount,
+      type: type,
+      category: category,
+      note: note,
+    });
+    res.status(200).json({ transaction });
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch transactions" });
   }
